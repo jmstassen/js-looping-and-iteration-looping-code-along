5581805deb9ab9f3d3dcd105ba59cfa534439d97
John M. Stassen <john.stassen@gmail.com>
2021-07-20 18:03:02 -0700

    commit tree da84d5644416befced27f8e080c39a3cd0178a71

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    var thanks = [];
+    for (let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return thanks;
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number)
+        number--
+    }
+}
